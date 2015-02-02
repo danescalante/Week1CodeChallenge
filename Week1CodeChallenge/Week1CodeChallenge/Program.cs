c15a0fe91519ae1cfdf4503ef2b8680a44c73131
Daniel Escalante <escalant.dan@gmail.com>
2015-02-02 15:23:50 -0700

    week1 code challenge
    a bunch of crying

--- Week1CodeChallenge/Week1CodeChallenge/Program.cs
+++ Week1CodeChallenge/Week1CodeChallenge/Program.cs
@@ -10,14 +10,48 @@ namespace Week1CodeChallenge
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i <= 20; i++)
+            {
+                Console.WriteLine(FizzBuzz(i));
+            }
+
+            for (int i = 92; i >= 79; i--)
+            {
+                Console.WriteLine(FizzBuzz(i));
+            }
+
+
+            Yodaizer("I like code");
+
         }
+     
         public static string FizzBuzz(int number)
         {
-            return string.Empty;
+            if ( (number % 5) == 0 && (number % 3) == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if ((number % 5) == 0)
+            {
+                return "Fizz";
+            }
+            else if ((number % 3) == 0)
+            {
+                return "Buzz";
+            }
+            return number.ToString();
         }
+
         public static string Yodaizer(string text)
         {
-            return string.Empty;
+           var someText = text;
+           string[] textArray = someText.Split(' ');
+           List<string> textList = textArray.ToList();
+            if (textList.Count == 2)
+            {
+             List<string>.textList.Insert(0, [3]);
+            }
+            
         }
         public static bool IsPrime(int number)
         {
@@ -26,6 +60,13 @@ namespace Week1CodeChallenge
         public static string DashInsert(int number)
         {
             return string.Empty;
+        }
+
+        static void TextStats(string input)
+        {
+            Console.WriteLine("Number of characters: " + input.Length);
+            Console.WriteLine("Number of Words: " + List<string> inputList = input.ToList());
+
         }
     }
 }
